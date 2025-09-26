0ce62cb017d5fa7aeee699ea5db5f4124456c72c
Void <vdev30301@gmail.com>
2025-09-26 18:06:26 -0300

    Atualizar o setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="sherma-ddos-tool",
+    name="sherma-tool",
     version="1.0.0",
-    description="Ferramenta para ataques dos/ddos.",
+    description="Ferramenta para ataques dos/ddos,
     author="vøidh7",
     packages=find_packages(),
     install_requires=[
         "requests>=2.31",
         "pyfiglet>=0.8"
     ],
-    entry_points={
-        "console_scripts": [
-            "sherma-tool = sherma.__main__:main"
-        ]
-    }
-name="sherma-Ddos-tool"
+) 
